cf2d5481724b4b6133b7df17ab5b0f552592b948
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2025-09-14 14:27:46 -0400

    Adding input from standard command line.

--- send.go
+++ send.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+
+		// If no argument is provided, print an error message
+		if len(os.Args) < 2 {
+			fmt.Println("Error: No message provided")
+			return
+		}
+
     fmt.Println("Sending a message.")
+		message := os.Args[1]
+		fmt.Println("Sending >> ", message)
+
 }
